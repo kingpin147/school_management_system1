f961427b08b8e61b9ffff0aac7cfac1000f08fd9
kingpin147 <nomiking0072012@gmail.com>
2025-10-21 15:41:52 +0500

    update

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <div className="h-screen flex">
+    {/* Left */}
+    <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
+      <Link href="/" ><Image src="logo.png" alt="logo image" width={32} height={32}/></Link>
+    </div>
+
+    {/* Right */}
+    <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">r</div>
+  </div>
+}
