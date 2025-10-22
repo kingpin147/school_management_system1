91338ad4cc2f7a5c927c85e5aae13dc98546835f
kingpin147 <nomiking0072012@gmail.com>
2025-10-22 15:18:29 +0500

    update

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,10 @@
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
+
+
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
@@ -7,11 +12,21 @@ export default function DashboardLayout({
 }>) {
   return <div className="h-screen flex">
     {/* Left */}
-    <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
-      <Link href="/" ><Image src="logo.png" alt="logo image" width={32} height={32}/></Link>
+    <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+      <Link href="/" 
+      className='flex items-center gap-2 justify-center lg:justify-start'>
+        <Image src="/logo.png" alt="SchoolLama" width={32} height={32}/>
+        <span className="hidden lg:block">SchoolLama</span>
+      </Link>
+      <Menu />
     </div>
 
     {/* Right */}
-    <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">r</div>
+    <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+      <Navbar/>
+      {children}
+
+    </div>
+
   </div>
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,33 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className=''>Navbar</div>
-  )
-}
+    <div className='flex items-center justify-between p-4'>
+      {/* Search Bar */}
+      <div className='hidden md:flex'>
+<Image src="/search.png" alt="search icon" width={14} height={14} />
+<input type="text" placeholder='Search...' />
 
-export default Navbar
+
+      </div>
+      {/* Icons and User */}
+      <div className='flex items-center gap-6'>
+        <div className='bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer'></div>
+          <Image src="/message.png" alt="message icon" width={20} height={20} />
+          <div className='flex items-center gap-6'>
+        <div className='bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer'>
+            <Image src="/announcement.png" alt="announcement icon" width={32} height={32} />
+          </div>
+          <div className='flex flex-col'>
+            <span className='text-xs leading-3 font-medium'>John Doe</span>
+            <span className=''>Admin</span>
+          </div>
+      </div>
+
+      
+      </div>
+  )
+};
+
+export default Navbar;
